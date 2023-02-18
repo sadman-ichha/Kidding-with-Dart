631e25ed02450608687f7468709b99ad4ba8208d
sadman_ichha <sadmanichha1@gmail.com>
2023-02-18 21:49:00 +0600

    List

--- Dart Language/list_in_dart.dart
+++ Dart Language/list_in_dart.dart
@@ -1,5 +1,10 @@
 void main() {
   List<int> myList = [5, 10, 15, 20, 25, 30];
-  var result = myList.length;
+  // var lengthResult = myList.length;
+  // var reversedResult = myList.reversed;
+  // var result = myList.last;
+  // var result = myList.single;
+  //  myList.add(35);
+  var result = myList.map((e) => print("I'm a ListMap"));
   print(result);
 }
