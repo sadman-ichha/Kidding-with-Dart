fb39321e81566c83ee3b85388d52e258a3e06505
sadman_ichha <sadmanichha1@gmail.com>
2023-02-19 13:04:08 +0600

    Enum

--- Dart Language/enum_in_dart.dart
+++ Dart Language/enum_in_dart.dart
@@ -1,8 +1,34 @@
+void main() {
+  final orderMethod = PaymentMethod.paypal;
+  switch (orderMethod) {
+    case PaymentMethod.Bkash:
+      print("Paying with Bkash");
+      break;
+    case PaymentMethod.Nagad:
+      print("Paying with Nagad");
+      break;
+    case PaymentMethod.Nagad:
+      print("Paying with Nagad");
+      break;
+    case PaymentMethod.applePay:
+      print("Paying with applePay");
+      break;
+    case PaymentMethod.creditCard:
+      print("Paying with creditCard");
+      break;
+    case PaymentMethod.paypal:
+      print("Paying with paypal");
+      break;
+
+    default:
+      print("Nothing Match");
+  }
+}
+
 enum PaymentMethod {
   Bkash,
   Nagad,
   creditCard,
   paypal,
   applePay,
-  googlePay,
 }
