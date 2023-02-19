0443b07c92a09dfe6307aaaf9d680acf11bcef48
sadman_ichha <sadmanichha1@gmail.com>
2023-02-19 11:27:04 +0600

    Set

--- Dart Language/set_in_dart.dart
+++ Dart Language/set_in_dart.dart
@@ -1,9 +1,15 @@
 void main() {
   Set mySet = {"A", "B", "C", "D", "F"};
-  Set mySetTwo = {5, 10, 15, 20, 3.1416, "D", "F"};
+  Set mySetTwo = {5, 10, 15, 20, 3.1416};
   // var result = mySet.difference(mySetTwo);
+
   // Union in Dart, you can use the "union" method to join two sets and get a new set that contains all
   //the elements from both sets. The "union" method is also known as the "join" method.
-  var result = mySet.union(mySetTwo);
+  //var result = mySet.union(mySetTwo);
+
+/* In Dart, you can use the "where" method to filter a set and create a new set that 
+ only contains elements that meet a specific condition.*/
+//
+  var result = mySet;
   print(result);
 }
