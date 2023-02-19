e2acb62979958fc2d7217b14ab3747ec733fda57
sadman_ichha <sadmanichha1@gmail.com>
2023-02-19 19:21:49 +0600

    function

---
+++ Dart Language/function_in_dart.dart
@@ -0,0 +1,19 @@
+void main() {
+  arrowFunction();
+}
+
+// Define a Function / normal function
+void addTwoNumber() {
+  print("addTwoNumber is ${10 + 50}");
+}
+
+// Arrow function
+arrowFunction() => print("This is Arrow function");
+
+
+  // returning function
+
+  // parameterized function
+  // optional positional parameter []
+  // optional named parameter {}
+  // higher order function
