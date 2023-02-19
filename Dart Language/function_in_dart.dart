de81aaad6bc16e6e8f961f88bc3729eb06dd9404
sadman_ichha <sadmanichha1@gmail.com>
2023-02-19 19:36:04 +0600

    Function

--- Dart Language/function_in_dart.dart
+++ Dart Language/function_in_dart.dart
@@ -1,5 +1,6 @@
 void main() {
-  arrowFunction();
+  parameterizedFunc(10, 20);
+  parameterizedFunc(20, 20);
 }
 
 // Define a Function / normal function
@@ -10,10 +11,18 @@ void addTwoNumber() {
 // Arrow function
 arrowFunction() => print("This is Arrow function");
 
+// returning function
+/* In Dart, a returning function is a function that returns a value of a specific data type. When 
+a Dart function is executed, it can perform some operations and then return a value to the caller.*/
+List<int> returningFunc() {
+  return [5, 10, 15, 20, 25];
+}
 
-  // returning function
-
-  // parameterized function
+// parameterized function
+parameterizedFunc(int num1, int num2) {
+  int result = num1 + num2;
+  print(result);
+}
   // optional positional parameter []
   // optional named parameter {}
   // higher order function
