57123ad690619768ba120b7ee62c5547f028a799
sadman_ichha <sadmanichha1@gmail.com>
2023-02-20 16:12:53 +0600

    Exception handling

--- Dart Language/exception_handling.dart
+++ Dart Language/exception_handling.dart
@@ -1,10 +1,16 @@
 void main() {
   // exception handling- try,on,catch,stack-trace,finally
+
+  // format exception
+
   try {
     String age = "10d";
     var result = int.parse(age);
     print(result);
-  } catch (e) {}
-  // format exception
+  } catch (e, StackTrace) {
+    print("Error is : $e");
+    print("library Error :\ $StackTrace");
+  }
+
   // Integer Division by Zero exception
 }
