37fa98ec0905c3f4290e681801bb2c7ab0166db4
sadman_ichha <sadmanichha1@gmail.com>
2023-02-20 16:47:26 +0600

    Exception Handling

--- Dart Language/exception_handling.dart
+++ Dart Language/exception_handling.dart
@@ -1,8 +1,7 @@
 void main() {
-  // exception handling- try,on,catch,stack-trace,finally
+  // exception handling- (try,on,catch,stack-trace,finally)
 
   // format exception
-
   try {
     String age = "10d";
     var result = int.parse(age);
@@ -12,5 +11,15 @@ void main() {
     print("library Error :\ $StackTrace");
   }
 
-  // Integer Division by Zero exception
+  // Integer Division by Zero Exception
+  try {
+    var result = 10 ~/ 0;
+    print(result);
+  } on IntegerDivisionByZeroException {
+    print("Can't Divide by Zero");
+  } catch (e) {
+    print(e);
+  } finally {
+    print("Finally Keyword Always Executed");
+  }
 }
