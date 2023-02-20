98d3a4e469c211e6207e70664a209bde309947e6
sadman_ichha <sadmanichha1@gmail.com>
2023-02-20 12:42:45 +0600

    Loop

--- Dart Language/loops_in_dart.dart
+++ Dart Language/loops_in_dart.dart
@@ -17,4 +17,23 @@ void main() {
       print(index);
     }
   }
+
+  // for in loop
+  List myList = [5, 10, 15, 20, 25, 30];
+  for (var element in myList) {
+    print("for in loop result : $element");
+  }
+
+  // for each loop
+  var forEachList = [
+    {"name": "Sadman"},
+    {"name": "Ichha"},
+    {"name": "Tom"},
+    {"name": "Oggy"}
+  ];
+  forEachList.forEach((element) {
+    print("forEach Loop result: ${element["name"]}");
+  });
+  // while
+  // do while
 }
