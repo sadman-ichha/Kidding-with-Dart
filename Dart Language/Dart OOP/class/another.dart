7ff57d96e202b9636933384f91b6008fb85a32f6
sadman_ichha <sadmanichha1@gmail.com>
2023-02-20 20:12:57 +0600

    class

---
+++ Dart Language/Dart OOP/class/another.dart
@@ -0,0 +1,20 @@
+class MyClass {
+  int age = 50;
+  String name = "Sadman";
+  List<int> myList = [5, 10, 15, 20, 25];
+
+  Map<String, dynamic> myMap = {
+    "name": "Ichha",
+    "name": "Tom",
+    "name": "Jerry",
+    "name": "Ben",
+  };
+
+  void myFuncOne() {
+    print("MyFunc One");
+  }
+
+  void myFuncTwo() {
+    print("MyFunc Two");
+  }
+}
---
+++ Dart Language/Dart OOP/class/class.dart
@@ -0,0 +1,7 @@
+import 'another.dart';
+
+void main() {
+  var obj = MyClass();
+  var result = obj.myList;
+  print(result);
+}
