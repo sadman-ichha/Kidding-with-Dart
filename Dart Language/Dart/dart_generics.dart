bbdf6c423514d54143a3103f4c80faa03b5a8e1b
sadman_ichha <sadmanichha1@gmail.com>
2023-04-15 12:03:00 +0600

    Casecade notation

---
+++ Dart Language/Dart/dart_generics.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  // Dart Generics
+
+  // Generics-> Homorogeneous <collaction_name>
+  List<int> userNo = [5, 10, 15, 20, 25, 30];
+  List<String> userName = ["Sadman", "kayif", "Jone"];
+  List<double> userCgpa = [3.13, 3.89, 2.89, 3.90];
+  List<bool> activeUser = [true, false, false, true];
+
+  // Generics-> Heterogeneous <collaction_name>
+  List myList = ["Sadman", 50, 60.0, true];
+  List<dynamic> myList1 = ["Sadman", 50, 60.0, true];
+
+  //Cascade notation ...
+}
