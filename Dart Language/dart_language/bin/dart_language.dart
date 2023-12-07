f3d368f29fd88126773bfdbdc077e000674abf13
sadman_ichha <sadmanichha1@gmail.com>
2023-12-07 19:28:53 +0600

    push

---
+++ Dart Language/dart_language/bin/dart_language.dart
@@ -0,0 +1,5 @@
+import 'package:dart_language/dart_language.dart' as dart_language;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_language.calculate()}!');
+}
---
+++ Dart Language/dart_language/test/dart_language_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_language/dart_language.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
